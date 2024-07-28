53513881db5c90202760710b52eefb35e9721489
elekere <elekere@outlook.com>
2024-07-28 16:16:21 +0100

    new commit

--- src/App.jsx
+++ src/App.jsx
@@ -35,22 +35,24 @@ function App() {
         </div>
         <button className="span-two">AC</button>
         <button>DEL</button>
-        {/* <DigitButton digit="/" dispatch={dispatch} /> */}
+        <OperationButton operation="/" dispatch={dispatch} />
 
         <DigitButton digit="1" dispatch={dispatch} />
         <DigitButton digit="2" dispatch={dispatch} />
         <DigitButton digit="3" dispatch={dispatch} />
-        <button>*</button>
+        <OperationButton operation="*" dispatch={dispatch} />
         <DigitButton digit="4" dispatch={dispatch} />
         <DigitButton digit="5" dispatch={dispatch} />
         <DigitButton digit="6" dispatch={dispatch} />
+        <OperationButton operation="+" dispatch={dispatch} />
         <button>+</button>
         <DigitButton digit="7" dispatch={dispatch} />
         <DigitButton digit="8" dispatch={dispatch} />
         <DigitButton digit="9" dispatch={dispatch} />
-        <button>-</button>
+        <OperationButton operation="-" dispatch={dispatch} />
         <DigitButton digit="." dispatch={dispatch} />
         <DigitButton digit="0" dispatch={dispatch} />
+        <OperationButton operation="/" dispatch={dispatch} />
         <button className="span-two"> =</button>
       </div>
     </>
