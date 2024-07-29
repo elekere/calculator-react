6e191a1ed6c0f7b09a17c68111f9abf1eef3d7aa
elekere <elekere@outlook.com>
2024-07-29 22:59:03 +0100

    new commit

--- src/App.jsx
+++ src/App.jsx
@@ -14,13 +14,28 @@ export const ACTIONS = {
 function reducer(state, { type, payload }) {
   switch (type) {
     case ACTIONS.ADD_DIGIT:
-      if (payload.digit == "0" && state.currentOperand === "0") return state;
-      if (payload.digit == "." && state.currentOperand.includes("."))
+      if (payload.digit == "0" && state.currentOperand === "0") {
         return state;
+      }
+      if (payload.digit == "." && state.currentOperand.includes(".")) {
+        return state;
+      }
       return {
         ...state,
         currentOperand: `${state.currentOperand || ""}${payload.digit}`,
       };
+    case ACTIONS.CHOOSE_OPERATION:
+      if (state.currentOperand == null && state.previousOperand == null) {
+        return state;
+      }
+      if (state.previousOperand == null) {
+        return {
+          ...state,
+          operation: payload.operation,
+          previousOperand: state.currentOperand,
+          currentOperand: null,
+        };
+      }
     case ACTIONS.CLEAR:
       return {};
   }
