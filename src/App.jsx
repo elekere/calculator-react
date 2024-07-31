49049eda193d1a2a12b83c79ec5b6c24515e16d0
elekere <elekere@outlook.com>
2024-07-31 22:52:11 +0100

    new commit

--- src/App.jsx
+++ src/App.jsx
@@ -148,16 +148,15 @@ function App() {
         <button onClick={() => dispatch({ type: ACTIONS.DELETE_DIGIT })}>
           DEL
         </button>
-        <OperationButton operation="÷" dispatch={dispatch} />
-
+        <OperationButton operation="+" dispatch={dispatch} />
         <DigitButton digit="1" dispatch={dispatch} />
         <DigitButton digit="2" dispatch={dispatch} />
         <DigitButton digit="3" dispatch={dispatch} />
-        <OperationButton operation="*" dispatch={dispatch} />
+        <OperationButton operation="÷" dispatch={dispatch} />
         <DigitButton digit="4" dispatch={dispatch} />
         <DigitButton digit="5" dispatch={dispatch} />
         <DigitButton digit="6" dispatch={dispatch} />
-        <OperationButton operation="+" dispatch={dispatch} />
+        <OperationButton operation="*" dispatch={dispatch} />
         <DigitButton digit="7" dispatch={dispatch} />
         <DigitButton digit="8" dispatch={dispatch} />
         <DigitButton digit="9" dispatch={dispatch} />
