81683657c96531fb30dc54aa80883aaddafa440b
elekere <elekere@outlook.com>
2024-07-31 22:20:09 +0100

    new commit

--- src/App.jsx
+++ src/App.jsx
@@ -71,6 +71,10 @@ function reducer(state, { type, payload }) {
       if (state.currentOperand.length === 1) {
         return { ...state, currentOperand: null };
       }
+      return {
+        ...state,
+        currentOperand: state.currentOperand.slice(0, -1),
+      };
 
     case ACTIONS.EVALUATE:
       if (
@@ -111,7 +115,15 @@ function evaluate({ currentOperand, previousOperand, operation }) {
   }
   return computation.toString();
 }
-
+const INTEGER_FORMATTER = new Intl.NumberFormat("en-us", {
+  maximumFractionDigits: 0,
+});
+function formatOperand(operand) {
+  if (operand == null) return;
+  const [integer, decimal] = operand.split(".");
+  if (decimal == null) return INTEGER_FORMATTER.format(integer);
+  return `${INTEGER_FORMATTER.format(integer)}.${decimal}`;
+}
 function App() {
   const [{ currentOperand, previousOperand, operation }, dispatch] = useReducer(
     reducer,
@@ -124,16 +136,18 @@ function App() {
         <div className="output">
           <div className="previous-operand">
             {" "}
-            {previousOperand} {operation}
+            {formatOperand(previousOperand)} {operation}
           </div>
-          <div className="current-operand">{currentOperand}</div>
+          <div className="current-operand">{formatOperand(currentOperand)}</div>
         </div>
         <button
           className="span-two"
           onClick={() => dispatch({ type: ACTIONS.CLEAR })}>
           AC
         </button>
-        <button>DEL</button>
+        <button onClick={() => dispatch({ type: ACTIONS.DELETE_DIGIT })}>
+          DEL
+        </button>
         <OperationButton operation="÷" dispatch={dispatch} />
 
         <DigitButton digit="1" dispatch={dispatch} />
